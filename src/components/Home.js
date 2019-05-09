6197ea0b4e3f74a70f2889686113eacc5005fd8a
Harmony <hkunzler@gmail..com>
2019-05-09 17:35:48 -0600

    updates

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,7 +6,7 @@ const Home = ({}) => (
   <div id='home'>
   <Row>
     <Col m={6} s={12}>
-      <Card style={{textAlign: 'center'}} header={<CardTitle />} actions={[<h5>Quick Links</h5>, <div className='quick-links'><a href='#'>Bio</a>, <a href='#'>Articles</a>, <a href='#'>Videos</a></div>]}>
+      <Card style={{textAlign: 'center'}} header={<CardTitle />} actions={[<h5>Quick Links</h5>, <div className='quick-links'><a href='#'>Bio</a> <a href='#'>Articles</a> <a href='#'>Videos</a></div>]}>
       <div className='home-red-box'>My Latest Offer For</div>
       <FlipClock
     type = "countdown"
