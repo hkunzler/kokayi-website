4ffe9fd7d757ea39f38a28229414de70280ea85a
Harmony <hkunzler@gmail..com>
2019-05-19 19:03:30 -0600

    added bio

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -5,11 +5,17 @@ const Bio = ({}) => (
     <div className='kokayi-profile'></div>
     <div className='side-bio'>
       <h3 style={{textAlign:'center', background: '#222222', color:'white', margin: '25px 2%', padding: '15px 0'}}>Bio</h3>
-      <p>King in the North. A good act does not wash out the bad, nor a bad act the good. Each should have its own reward. House Tarly of Horn Hill It's ten thousand miles between Kings landing and the wall. I pledge my life and honor to the Night's Watch, for this night and all the nights to come.</p>
+      <p>Each year, three neighborhoods in Anchorage, Alaska vy for which one is considered the most ethnically diverse neighborhood inside the United States. Journalist and community organizer, Kokayi Nosakhere graduated high school from one of those neighborhoods. In 1995, his attendance at the historic, Million Man March, set the course of his life, engaging in social justice work.</p>
     <br/>
-    <p>Tubi daor. Avy jorraelan. Nuhor lir gurenna. Toli rhuqo lotinti, kostilus. Valar morghulis.</p>
+    <p>With a degree in American history secured from the University of Alaska, Anchorage (UAA), helped him leverage statewide attention towards school meals legislation, as he fasted for 28 days on the Juneau Hunger Strike (2012). The goal was to win $1.9 million, the amount needed to cover the gap between what parents could pay and the federal government subsidized in the school nutrition program. The Juneau Hunger Strike won a $3 million payment.</p>
     <br/>
-    <p>Es havazhaan. Athdavrazar! Ezas eshna gech ahilee! Hash anha atihak yera save? Khal ahhas arakh.</p>
+    <p>2012 also saw the creation of Alaska Commons, a grassroots, digital current event magazine born out of a legislative failure defending LGBTQ civil rights. Mayor Dan Sullivan refused to distribute sufficient ballots to polling locations. The initiative failed. Nosakhere was a founding member of Alaska Commons, learning journalism, on the fly. In the five years Alaska Commons actively published, Nosakhere earned two Alaska Press Awards.</p>
+    <br/>
+    <p>In 2018, he earned the Alaska FBI Director’s Community Leadership Award for his 2017 anti-violence work in Mt. View.</p>
+    <br/>
+    <br/>
+    <p>Nosakhere currently resides in Ashland, Oregon, where he has completed phase one of his plan to to launch a “Love Letter for America” campaign nationwide. He is 45 years old with two daughters.</p>
+
     </div>
   </div>
 );
