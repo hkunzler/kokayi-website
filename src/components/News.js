3c3aa4b3613dcf4b59759b22263357ebac46c307
Harmony <hkunzler@gmail..com>
2019-05-21 18:33:46 -0600

    reorganizing articles

--- src/components/News.js
+++ src/components/News.js
@@ -5,13 +5,13 @@ const News = ({}) => (
   <div id='news'>
     <h3 style={{textAlign:'center', background: '#222222', color:'white', margin: '0 2%', padding: '15px 0'}}>News</h3>
     <div className='grid'>
-    <Row>
-      <Col m={6} s={12}>
-        <Card horizontal="horizontal" header={<CardTitle />} actions={[<h5>Ashland community kindness project gains national attention</h5>, <small>by Jennevieve Fong <br/> Feb 4, 2019<br/><br/></small>, <a href='https://ktvl.com/news/local/ashland-community-kindness-project-gains-national-attention'>Read Here</a>]}>
-          <div className='ashland'></div>
-        </Card>
-      </Col>
-    </Row>
+      <Row>
+        <Col m={6} s={12}>
+          <Card horizontal="horizontal" header={<CardTitle />} actions={[<h5>49 Voices: Kokayi Nosakhere of Anchorage</h5>, <small>by  Wesley Early <br/> Feb 24, 2017<br/><br/></small>, <a href='https://www.alaskapublic.org/2017/02/24/49-voices-kokayi-nosakhere-of-anchorage/'>Read Here</a>]}>
+            <div className='voices'></div>
+          </Card>
+        </Col>
+      </Row>
     <Row>
       <Col m={6} s={12}>
         <Card horizontal="horizontal" header={<CardTitle />} actions={[<h5>2017 Director’s Community Leadership Award Recipient</h5>, <small>2017<br/><br/></small>, <a href='https://www.fbi.gov/about/community-outreach/dcla/2017/anchorage-kokayi-nosakhere'>Read Here</a>]}>
@@ -26,10 +26,11 @@ const News = ({}) => (
         </Card>
       </Col>
     </Row>
+
     <Row>
       <Col m={6} s={12}>
-        <Card horizontal="horizontal" header={<CardTitle />} actions={[<h5>49 Voices: Kokayi Nosakhere of Anchorage</h5>, <small>by  Wesley Early <br/> Feb 24, 2017<br/><br/></small>, <a href='https://www.alaskapublic.org/2017/02/24/49-voices-kokayi-nosakhere-of-anchorage/'>Read Here</a>]}>
-          <div className='voices'></div>
+        <Card horizontal="horizontal" header={<CardTitle />} actions={[<h5>Ashland community kindness project gains national attention</h5>, <small>by Jennevieve Fong <br/> Feb 4, 2019<br/><br/></small>, <a href='https://ktvl.com/news/local/ashland-community-kindness-project-gains-national-attention'>Read Here</a>]}>
+          <div className='ashland'></div>
         </Card>
       </Col>
     </Row>
