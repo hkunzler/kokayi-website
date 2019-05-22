2f34b0ec7edfc61f7f9493c812b38c779a099830
Harmony <hkunzler@gmail..com>
2019-05-21 18:28:17 -0600

    updated home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,13 +7,10 @@ const Home = ({}) => (
   <Row>
     <Col m={6} s={12}>
       <Card style={{textAlign: 'center'}} header={<CardTitle />} actions={[<h5>Quick Links</h5>, <div className='quick-links'><a href='#bio'>Bio</a> <a href='#articles'>Articles</a> <a href='#videos'>Videos</a> <a href='#news'>News</a> </div>]}>
-      <div className='home-red-box'>My Latest Offer For</div>
-      <FlipClock
-    type = "countdown"
-    count_to = "2019-12-25 00:00:00"
- />
+      <div className='home-red-box'>Kokayi On Tour</div>
+      <div className='tour'>06 . 01 . 2019 </div>
       <div className='kokayi-fist'></div>
-      <div className='top-right-text'>Purchase Theme</div>
+      <div className='top-right-text'>Reach Out</div>
       </Card>
     </Col>
   </Row>
